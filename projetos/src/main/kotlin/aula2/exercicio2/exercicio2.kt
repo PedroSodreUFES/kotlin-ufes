c5672a680e5dda24d8fce1a87264673e6faefa01
PedroSodreUFES <pedro.malini@edu.ufes.br>
2026-08-20 18:51:28 -0300

    feat: aula 2 exercício 2

--- projetos/src/main/kotlin/aula2/exercicio2/exercicio2.kt
+++ projetos/src/main/kotlin/aula2/exercicio2/exercicio2.kt
@@ -6,15 +6,28 @@ fun main() {
     print("Digite seu texto: ")
     val text = readln()
     val textMap: MutableMap<String, Int> = mutableMapOf()
+    var treatedText: String = ""
 
-    for(word in text.split(" ")) {
+    for (character in text.toCharArray()) {
+        if(character.isLetter() || character == ' ') {
+            treatedText += character
+        }
+    }
+
+    for(word in treatedText.split(" ")) {
         var key = word.lowercase()
         if(textMap.containsKey(key)) {
             var n = textMap[key]!!
             n+=1
             textMap[key] = n
         } else {
-            textMap[key] = 0
+            textMap[key] = 1
         }
+    }
+
+    val mapList = textMap.toList().sortedBy { (_, value) -> value }.reversed()
+
+    repeat(number) { i ->
+        println("${mapList[i].first}: ${mapList[i].second}")
     }
 }
